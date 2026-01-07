9c6a3a6e15a81854b650ce52a023509c1d763516
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-08 00:56:01 +0530

    Dynamic Values in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,26 @@ export const App = () => {
 
 
   const NetflixSeries = () => {
+
+    const name = "Stranger Things";
+    const rating = 4.7;
+    const summary =
+     "Stranger Things is a sci-fi thriller series set in the 1980s. It follows a group of kids in the town of Hawkins whose lives change when a boy named Will disappears.Together, they fight supernatural creatures and government conspiracies while protecting their friends and families.";
+
+     const returnGenre = () => {
+      const genre = "Horror";
+      return genre;
+     }
+
     return(
        <div>
       <div>
         <img src="stranger.png" alt="st" width="40%" height="40%x" />
       </div>
-      <h2>Name: Stranger Things</h2>
-      <h3>Rating: 4.7</h3>
-      <p> Summary: Stranger Things is a sci-fi thriller series set in the 1980s. It follows a group of kids in the town of Hawkins whose lives change when a boy named Will disappears.Together, they fight supernatural creatures and government conspiracies while protecting their friends and families.</p>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {2+2.8}</h3>
+      <p> Summar: {summary}</p>
+      <p>Genre: {returnGenre()}</p>
     </div>
     )
   }
