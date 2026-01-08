575ca34eff795625390c3cf638ddc367e33dc3db
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-08 23:17:17 +0530

    data transfer using props

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,31 +1,12 @@
 import seriesData from "../api/seriesData";
+import SeriesCard from "./SeriesCard";
 
 const NetflixSeries = () => {
   return (
     <ul>
       {seriesData.map((curElem) => {
-        return (
-          <li key={curElem.id}>
-            <div>
-              <img
-                src={curElem.img_url}
-                alt={curElem.name}
-                width="40%"
-                height="40%x"
-              />
-            </div>
-            <h2>Name: {curElem.name}</h2>
-            <h3>Rating: {curElem.rating}</h3>
-            <p> Summar: {curElem.description}</p>
-            <p>Genre: {curElem.genre}</p>
-            <p>Cast: {curElem.cast}</p>
-            <a href={curElem.watch_url} target="_blank">
-              <button>Watch Now</button>
-            </a>
-          </li>
-        );
+        return <SeriesCard key={curElem.id} curElem={curElem} />;
       })}
-      ;
     </ul>
   );
 };
