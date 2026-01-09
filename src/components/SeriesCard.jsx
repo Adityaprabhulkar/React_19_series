c790f64367a6e5122b2a783df4c1014b7ef3630c
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-09 21:30:36 +0530

    using css modules

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,19 +1,27 @@
 export const SeriesCard = ({ data }) => {
-  const {  img_url, name, rating, description, cast, genre, watch_url } =
+  const { img_url, name, rating, description, cast, genre, watch_url } =
     data;
+
+   const ratingClass = rating >= 8.5 ? "super_hit" : "avarage";
   return (
     <li className="series-card">
       <div className="card-img">
         <img src={img_url} alt={name} width="40%" height="40%x" />
       </div>
-      <h3 className="card-title">Name: {name}</h3>
-      <p>Rating: {rating}</p>
+      <div>
+        <h3 className="card-title">Name: {name}</h3>
+      <h3>
+        Rating: <span className={`rating ${ratingClass} `}>
+                  {rating}
+                </span>
+      </h3>
       <p className="card-desc"> Summary: {description}</p>
-      <p>Genre: {genre}</p>
-      <p>Cast: {cast}</p>
+      <p>Genre: {genre.join(", ")}</p>
+      <p>Cast: {cast.join(", ")}</p>
       <a href={watch_url} target="_blank">
         <button className="btn">Watch Now</button>
       </a>
+      </div>
     </li>
   );
 };
