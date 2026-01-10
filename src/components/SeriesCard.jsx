e5444ae227fd8cd515d1e591f765738abc80774b
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-10 16:01:45 +0530

    styled component using styled-components

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,8 +1,36 @@
-export const SeriesCard = ({ data }) => {
-  const { img_url, name, rating, description, cast, genre, watch_url } =
-    data;
+import styled from "styled-components";
 
-   const ratingClass = rating >= 8.5 ? "super_hit" : "avarage";
+export const SeriesCard = ({ data }) => {
+  const { img_url, name, rating, description, cast, genre, watch_url } = data;
+
+  // const btn = {
+  // const ButtonStyle = styled.button({
+  //   padding: "1.2rem 2.4rem",
+  //   border: "none",
+  //   fontSize: "1.6rem",
+  //   backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+  //   color: "var(--btn-color)",
+  //   fontWeight: "bold",
+  //   cursor: "pointer",
+  // });
+
+   const ButtonStyle = styled.button`
+    padding: 1.2rem 2.4rem;
+    border: none;
+    font-size: 1.6rem;
+    background-color: ${(props) => props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+    color: var(--btn-color);
+    fontWeight: bold;
+    cursor: pointer;
+  `;
+
+  const Rating = styled.h3`
+  font-size: 1.6rem;
+  color: #7dcea0;
+  text-transform: capitalize;
+  `;
+
+  const ratingClass = rating >= 8.5 ? "super_hit" : "avarage";
   return (
     <li className="series-card">
       <div className="card-img">
@@ -10,17 +38,16 @@ export const SeriesCard = ({ data }) => {
       </div>
       <div>
         <h3 className="card-title">Name: {name}</h3>
-      <h3>
-        Rating: <span className={`rating ${ratingClass} `}>
-                  {rating}
-                </span>
-      </h3>
-      <p className="card-desc"> Summary: {description}</p>
-      <p>Genre: {genre.join(", ")}</p>
-      <p>Cast: {cast.join(", ")}</p>
-      <a href={watch_url} target="_blank">
-        <button className="btn">Watch Now</button>
-      </a>
+        <Rating>
+          Rating: <span className={`rating ${ratingClass} `}>{rating}</span>
+        </Rating>
+        <p className="card-desc"> Summary: {description}</p>
+        <p>Genre: {genre.join(", ")}</p>
+        <p>Cast: {cast.join(", ")}</p>
+        <a href={watch_url} target="_blank">
+          {/* <button className="btn">Watch Now</button> */}
+          <ButtonStyle rating={rating}>Watch Now</ButtonStyle>
+        </a>
       </div>
     </li>
   );
