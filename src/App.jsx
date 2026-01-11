16f2bfbb84cb76b44096a75c3221ccc4904fe453
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-11 17:22:30 +0530

    Using event handlers in function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import NetflixSeries from "./components/NetflixSeries";
+// import NetflixSeries from "./components/NetflixSeries";
 // import Profile from "./components/Profile";
 import "./components/Netflix.css"
+import { EventHandling } from "./components/EventHandling";
 
 export const App = () => {
   return (
     <section className="container">
-      <h1 className="card-heading">List of Best Netflix Series</h1>
-      <NetflixSeries />;
+      {/* <h1 className="card-heading">List of Best Netflix Series</h1>
+      <NetflixSeries />; */}
+      <EventHandling/>
     </section>
   )
   //  <Profile/>
---
+++ src/components/EventHandling.jsx
@@ -0,0 +1,48 @@
+import "./EV.css";
+
+export const EventHandling = () => {
+  // function handleBUttonClick () {
+  //     alert("Hey i am onClick event");
+  // }
+
+  const handleBUttonClick = (event) => {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.type);
+
+    alert("Hey i am onClick event");
+  };
+
+
+  const handleWelcomeUser = (user) => {
+    console.log(`Hey ${user} Welcome here`);
+    
+  }
+ 
+  return (
+    <>
+      {/* Function components with named Functions */}
+      <button onClick={handleBUttonClick}>Click Me </button>
+      <br />
+
+      <button onClick={(event) => handleBUttonClick(event)}>Click Me2 </button>
+
+      <br/>
+        {/* Inline Evnent Handler */}
+      <button onClick={(event) => console.log(event)}> Inline function </button>
+         <br/>
+        {/* Function Components with inline Arrow Functions */}
+
+        <button onClick={() => alert('Hey i am inline event function')}>Inline Arrow</button>
+
+        <br/>
+
+        {/* <button onClick={handleWelcomeUser('Aditya')}>Click Me</button> */}
+        <button onClick={()=>handleWelcomeUser('Aditya')}>Click Me</button>
+        <button onClick={()=>handleWelcomeUser('Raj')}>Click Me</button>
+        <button onClick={()=>handleWelcomeUser('Yatin')}>Click Me</button>
+
+
+    </>
+  );
+};
