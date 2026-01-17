53fee607d6bdc1b09b6e7a525dd017bad03a8e67
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-17 16:44:22 +0530

    complete event propagation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 // import Profile from "./components/Profile";
 import "./components/Netflix.css"
 // import { EventHandling } from "./components/EventHandling";
-import { EventProps } from "./components/EventProps";
+// import { EventProps } from "./components/EventProps";
+import EventPropagation from "./components/EventPropagation";
 
 export const App = () => {
   return (
@@ -10,7 +11,8 @@ export const App = () => {
       {/* <h1 className="card-heading">List of Best Netflix Series</h1>
       <NetflixSeries />; */}
       {/* <EventHandling/> */}
-      <EventProps/>
+      {/* <EventProps/> */}
+      <EventPropagation/>
 
     </section>
   )
--- src/components/EventPropagation.jsx
+++ src/components/EventPropagation.jsx
@@ -1,4 +1,4 @@
-
+import "./EV.css";
 
 const EventPropagation = () => {
     const handleGrandParent = () => {
@@ -9,18 +9,21 @@ const EventPropagation = () => {
         console.log("Parent clicked");
     }
 
-    const handleChildClick = () => {
-        console.log("Child element clicked");
+    const handleChildClick = (event) => {
+        console.log(event);
+        event.stopPropagation();
+        console.log("Child  clicked");
     }
     
+   
 
 
 
   return (
     <section className='main-div'>
-        <div className='g-div' onClick={handleGrandParent}>
-            <div className='p-div' onClick={handleParentClick}>
-                <button className='c-div' onClick={handleChildClick}>
+        <div className='g-div' onClickCapture={handleGrandParent}>
+            <div className='p-div' onClickCapture={handleParentClick}>
+                <button className='c-div' onClickCapture={handleChildClick}>
                     Child Div
                 </button>
             </div>
